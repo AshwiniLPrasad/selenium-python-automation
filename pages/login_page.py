0ce04fd3bbf6b0bbb0ec26636c2f923dc73a89f1
AshwiniLPrasad <ashwinilprasad97@gmail.com>
2026-08-29 12:41:31 +0530

    Add login functionality to LoginPage class
    Implemented methods for username, password entry, and login button click.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,2 +1,20 @@
+from selenium.webdriver.common.by import By
+
+
 class LoginPage:
-    pass
+
+    USERNAME = (By.ID, "user-name")
+    PASSWORD = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def enter_username(self, username):
+        self.driver.find_element(*self.USERNAME).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(*self.PASSWORD).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(*self.LOGIN_BUTTON).click()
